0c39906460287ca395c4031d68d1c60aa0395571
Mohamed Mousa <mousa1152003@gmail.com>
2026-01-18 01:51:38 +0200

    finish language

--- lib/features/categories/presentation/widgets/custom_list_hader_categorie.dart
+++ lib/features/categories/presentation/widgets/custom_list_hader_categorie.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task_trust_development/core/styles/colors.dart';
 import 'package:task_trust_development/core/styles/styles.dart';
 import '../../../../core/components/custom_cached_circleImage.dart';
+import '../../../../core/enum/enum_language.dart';
+import '../../../profile/presentation/view_model/lanage_cubit.dart';
 import '../../data/models/categories_dto.dart';
 
 class CustomListHeaderCategories extends StatefulWidget {
-  final List<DtoCategories> categories;
+  final dynamic categories;
   final Function(DtoCategories)? onTapItem;
 
   const CustomListHeaderCategories({
@@ -53,8 +56,9 @@ class _CustomListHeaderCategoriesState extends State<CustomListHeaderCategories>
                 children: [
                   CustomCachedCircleImage(imageUrl: category.image ?? ""),
                   SizedBox(width: 8.w),
-                  Text(
-                    category.nameEn ?? "",
+                  Text( context.watch<LanguageCubit>().state == Language.en
+                          ?   category.nameEn
+                          : category.nameAr,
                     style: isSelected ? AppTextStyles.textStyle18 : AppTextStyles.textStyle18.copyWith(
                       color: MyColors.grey
                     )
--- lib/features/navigation_bar_screen/navigation_screens.dart
+++ lib/features/navigation_bar_screen/navigation_screens.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:task_trust_development/features/cart/presentation/pages/cart_view.dart';
 import 'package:task_trust_development/features/categories/presentation/pages/categories_view.dart';
 import 'package:task_trust_development/features/profile/presentation/pages/profile_view.dart';
 
 final List<Widget> navigationScreens = [
   CategoriesView(),
-  CartView(),
   ProfileView(),
 ];
